d16365b4f7cbd62ac8926b5df436b5d71fe517ef
Amitkr77 <amitroyk99@gmail.com>
2025-02-20 16:49:59 +0530

    imporve the render logic for Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import ContactUs from "./pages/ContactUs";
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchList from "./pages/SeacrhList";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import AboutUs from "./pages/AboutUs";
 import Hotels from "./pages/category/Hotels";
 import Property from "./pages/category/Property";
@@ -15,41 +13,169 @@ import VendorSignin from "./Authentication/Vendor/VendorSignIn";
 import ProductPage from "./pages/ProductPage";
 import VendorDashboard from "./pages/vendor/VendorDashboard";
 import UserDashboard from "./pages/user/UserDashboard";
-import HotelSection from "./components/HotelSection";
-import InteriorSection from "./components/InteriorSection";
-import PropertySection from "./components/PropertySection";
 import ExploreSection from "./components/ExploreSection";
 import TestGrid from "./components/TestGrid";
 import PropertyPost from "./components/PropertyPost";
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/Contact" element={<ContactUs />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/search" element={<SearchList />} />
-          <Route path="/property" element={<Property />} />
-          <Route path="/interior" element={<InteriorPage />} />
-          <Route path="/hotel" element={<Hotels />} />
-          <Route path="/vendor/signin" element={<VendorSignin />} />
-          <Route path="/vendor/signup" element={<VendorSignup />} />
-          <Route path="/vendor/dashboard" element={<VendorDashboard />} />
-          <Route path="/user/signin" element={<UserSignin />} />
-          <Route path="/user/signup" element={<UserSignup />} />
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/explore" element={<ExploreSection />} />
-          <Route path="/testgrid" element={<TestGrid />} />
-          <Route path="/property/post" element={<PropertyPost />} />
-        </Routes>
-        <Footer />
-      </Router>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-      
+function App() {
+  // Main Layout (with Navbar and Footer)
+  const MainLayout = ({ children }) => (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
     </>
+  );
+
+  // No Layout (no Navbar or Footer)
+  const NoLayout = ({ children }) => <>{children}</>;
+  return (
+    <Router>
+      <Routes>
+        {/* Routes with Main Layout */}
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <LandingPage />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/Contact"
+          element={
+            <MainLayout>
+              <ContactUs />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/aboutus"
+          element={
+            <MainLayout>
+              <AboutUs />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <MainLayout>
+              <SearchList />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/property"
+          element={
+            <MainLayout>
+              <Property />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/interior"
+          element={
+            <MainLayout>
+              <InteriorPage />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/hotel"
+          element={
+            <MainLayout>
+              <Hotels />
+            </MainLayout>
+          }
+        />
+        
+        <Route
+          path="/user/signup"
+          element={
+            <NoLayout>
+              <UserSignup />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <MainLayout>
+              <ProductPage />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/explore"
+          element={
+            <MainLayout>
+              <ExploreSection />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/testgrid"
+          element={
+            <NoLayout>
+              <TestGrid />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/property/post"
+          element={
+            <MainLayout>
+              <PropertyPost />
+            </MainLayout>
+          }
+        />
+
+        {/* Routes without Layout (no Navbar or Footer) */}
+        <Route
+          path="/user/dashboard"
+          element={
+            <NoLayout>
+              <UserDashboard />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/user/signin"
+          element={
+            <NoLayout>
+              <UserSignin />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/vendor/signin"
+          element={
+            <NoLayout>
+              <VendorSignin />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/vendor/signup"
+          element={
+            <NoLayout>
+              <VendorSignup />
+            </NoLayout>
+          }
+        />
+        <Route
+          path="/vendor/dashboard"
+          element={
+            <NoLayout>
+              <VendorDashboard />
+            </NoLayout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
